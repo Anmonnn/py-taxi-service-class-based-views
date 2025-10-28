3af2eca4defab8ea9e80e83d86bd095d1410bd6e
Білоус Антон <a.bilous2@avrora.ua>
2025-10-28 23:25:09 +0200

    save

--- taxi/views.py
+++ taxi/views.py
@@ -43,4 +43,4 @@ class DriverListView(generic.ListView):
 class DriverDetailView(generic.DetailView):
     model = Driver
     context_object_name = "driver_detail"
-    queryset = Driver.objects.prefetch_related("cars").all()
+    queryset = Driver.objects.prefetch_related("cars__manufacturer").all()
